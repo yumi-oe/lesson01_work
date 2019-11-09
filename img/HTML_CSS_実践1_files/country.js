4256b492c1979a903910d021b809bc51fa9f48ca
yumi-oe <yumioe1125@gmail.com>
2019-11-09 12:13:53 +0900

    init

---
+++ img/HTML_CSS_実践1_files/country.js
@@ -0,0 +1,12 @@
+(function () {
+	var country = {'code':'jp','id':46,'defaultLangauge':'ja'};
+	
+	if (!window.evidon) window.evidon = {};
+	
+	if (window.evidon.notice) {
+		window.evidon.notice.setLocation(country);
+	}
+	else {
+		window.evidon.location = country;
+	}
+})();
